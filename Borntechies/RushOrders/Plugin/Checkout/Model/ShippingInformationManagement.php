3ffde0839a6516b27915ed07832a509e48ff02a6
Jaydeep Patel <jaydeeppatelse@gmail.com>
2024-12-31 14:58:05 +0530

    Add Rush Order Extension

---
+++ Borntechies/RushOrders/Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -0,0 +1,51 @@
+<?php
+namespace Borntechies\RushOrders\Plugin\Checkout\Model;
+
+
+class ShippingInformationManagement
+{
+    /**
+     * @var \Magento\Quote\Model\QuoteRepository
+     */
+    protected $quoteRepository;
+
+    /**
+     * @var \Borntechies\RushOrders\Helper\Data
+     */
+    protected $dataHelper;
+
+    /**
+     * @param \Magento\Quote\Model\QuoteRepository $quoteRepository
+     * @param \Borntechies\RushOrders\Helper\Data $dataHelper
+     */
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        \Borntechies\RushOrders\Helper\Data $dataHelper
+    )
+    {
+        $this->quoteRepository = $quoteRepository;
+        $this->dataHelper = $dataHelper;
+    }
+
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    )
+    {
+        $customFee = $addressInformation->getExtensionAttributes()->getFee();
+        $quote = $this->quoteRepository->getActive($cartId);
+        if ($customFee) {
+            $fee = $this->dataHelper->getCustomFee();
+            $quote->setFee($fee);
+        } else {
+            $quote->setFee(NULL);
+        }
+    }
+}
+
